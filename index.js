6afb32a47e4140ea4e6eb5c0a1dcac7b20c9c001
twinh <twinhuang@qq.com>
2021-07-04 00:52:36 +0800

    refactor(fower-preset): 简化生成间距和字体大小

--- index.js
+++ index.js
@@ -2,59 +2,34 @@
 import '@fower/taro';
 import {setConfig, getConfig} from '@fower/core';
 
+const spacing = (size) => {
+  return (size * 0.25) + 'rem';
+};
+
+// NOTE: prop 目前不支持小数，CSS 变量支持
+let spacings = {};
+[0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 96].map(size => {
+  spacings[size] = spacing(size);
+});
+
 setConfig({
   theme: {
-    spacings: {
-      0: '0',
-      // NOTE: prop 目前不支持小数，CSS 变量支持
-      0.5: '0.125rem',
-      1: '0.25rem',
-      1.5: '0.375rem',
-      2: '0.5rem',
-      2.5: '0.625rem',
-      3: '0.75rem',
-      3.5: '0.875rem',
-      4: '1rem',
-      5: '1.25rem',
-      6: '1.5rem',
-      7: '1.75rem',
-      8: '2rem',
-      9: '2.25rem',
-      10: '2.5rem',
-      12: '3rem',
-      14: '3.5rem',
-      16: '4rem',
-      20: '5rem',
-      24: '6rem',
-      28: '7rem',
-      32: '8rem',
-      36: '9rem',
-      40: '10rem',
-      44: '11rem',
-      48: '12rem',
-      52: '13rem',
-      56: '14rem',
-      60: '15rem',
-      64: '16rem',
-      72: '18rem',
-      80: '20rem',
-      96: '24rem',
-    },
+    spacings,
     fontSizes: {
-      xs: '0.75rem',
-      sm: '0.875rem',
+      xs: spacing(3),
+      sm: spacing(3.5),
       // NOTE: 暂无 md，先用 base
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '3.75rem',
-      '7xl': '4.5rem',
-      '8xl': '6rem',
-      '9xl': '8rem',
+      base: spacing(4),
+      lg: spacing(4.5),
+      xl: spacing(5),
+      '2xl': spacing(6),
+      '3xl': spacing(7.5),
+      '4xl': spacing(9),
+      '5xl': spacing(12),
+      '6xl': spacing(15),
+      '7xl': spacing(18),
+      '8xl': spacing(24),
+      '9xl': spacing(32),
     },
     colors: (() => {
       const colors = {
